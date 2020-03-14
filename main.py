ad9a5d117cee59a381941c78c7db0f74026656eb
Fweak <freakgamer2331@gmail.com>
2020-03-13 19:16:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,33 +4,38 @@ init(convert=True)
 
 def start():
 
-    url = "https://discordservers.me/servers/search?term=Nitro&page="
+    urls = ["https://discordservers.me/servers/search?term=Nitro&page=", "https://discordservers.me/servers/search?term=Giveaway&page="]
     numb = 1
     session = requests.Session()
     print(f"[{Fore.CYAN}>{Fore.RESET}] Input Discord Token")
     token = input(" > ")
 
-    while int(numb) < 100:
-        time.sleep(2)
-        response = session.get(url + str(numb)).text
+    while True:
+        if numb < 100:
+            for url in urls:
+                response = session.get(url + str(numb))
+                if response.status_code != 404:
+                    regex = re.search("https(:)\/\/discord.gg\/[a-zA-Z0-9]+", response.text)
 
-        regex = re.search("https(:)\/\/discord.gg\/[a-zA-Z0-9]+", response)
+                    if regex:
+                        code = str(regex.group()).split("/")[3]
+                        
+                        headers = {
+                            "Authorization": token
+                        }
 
-        if regex:
-            code = str(regex.group()).split("/")[3]
-            
-            headers = {
-                "Authorization": token
-            }
-
-            inviteResp = session.post(f"https://discordapp.com/api/v6/invites/{code}", headers=headers).json()
-            try:
-                if inviteResp["guild"]["name"]:
-                    print(f"[{Fore.CYAN}Success{Fore.RESET}] Joined the server: {inviteResp['guild']['name']}")
-            except:
-                pass
+                        inviteResp = session.post(f"https://discordapp.com/api/v6/invites/{code}", headers=headers).json()
+                        try:
+                            if inviteResp["guild"]["name"]:
+                                print(f"[{Fore.CYAN}Success{Fore.RESET}] Joined the server: {inviteResp['guild']['name']}")
+                        except:
+                            pass
+                    else:
+                        pass
+                elif response.status_code == 404:
+                    break
         else:
-            pass
+            break
         numb+=1
     input("")
 
